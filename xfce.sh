3dc1eafb0571ddb962758136c6cadf9b4deddba6
Johannes Kamprad <archlinux@kamprad.net>
2020-01-09 19:13:44 +0100

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,55 +1,36 @@
 #bin/bash
 
-Main() {
-    local dotfiles=https://github.com/endeavouros-team/DE-theming.git
-    local greeter=https://github.com/endeavouros-team/EndeavourOS-archiso/raw/master/airootfs/etc/lightdm/lightdm-gtk-greeter.conf
-    local packages=(
-        arc-gtk-theme
-        arc-x-icons-theme
-        lightdm
-        lightdm-gtk-greeter
-        lightdm-gtk-greeter-settings
-    )
-    local greeterfile=$(basename $greeter)
+echo "******* Installing EndeavourOS Theming for XFCE4 *******"
 
-    local workdir=$(mktemp)
-    pushd $workdir >/dev/null           # doing all here at temporary folder
+echo "******* Getting theme packages installed now: *******" && sleep 1
+    sudo pacman -S arc-gtk-theme arc-x-icons-theme lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings --noconfirm --needed
 
-    echo "******* Installing EndeavourOS Theming for XFCE4 *******"
+echo "******* setting up theme for Light-DM: *******" && sleep 1
+    wget -q --timeout=10 https://github.com/endeavouros-team/EndeavourOS-archiso/raw/master/airootfs/etc/lightdm/lightdm-gtk-greeter.conf
+        sudo cp  lightdm-gtk-greeter.conf /etc/lightdm/
+        rm lightdm-gtk-greeter.conf
 
-    # echo "******* Getting theme packages installed now: *******" && sleep 1
-    sudo pacman -S "${packages[@]}" --noconfirm --needed >& /dev/null
-
-    # echo "******* setting up theme for Light-DM: *******" && sleep 1
-    wget -q --timeout=10 $greeter && {
-        sudo cp $greeterfile /etc/lightdm/
-        rm $greeterfile
-    } || {
-        echo "Error: sorry, unable to fetch $greeterfile" >&2
-        return 1
-    }
-
-    # echo "******* cloning dotfiles for EndeavourOS - XFCE4 Theming *******" && sleep 1
-    git clone $dotfiles >& /dev/null
-    cd dotfiles
-    sudo cp -R endeavouros /usr/share/
-    rm -R ~/.config/Thunar ~/.config/qt5ct ~/.config/xfce4
-    cp -R XFCE/. ~/
-    dconf load / /dev/null
-    rm -rf $workdir
+echo "******* cloning dotfiles for EndeavourOS - XFCE4 Theming *******" && sleep 1
     wget https://raw.githubusercontent.com/endeavouros-team/liveuser-desktop-settings/master/dconf/mousepad.dconf
     dbus-launch dconf load / < mousepad.dconf
     rm mousepad.dconf
+    git clone https://github.com/endeavouros-team/DE-theming.git
+    cd DE-theming
+    rm -rf ~/.config/Thunar ~/.config/qt5ct ~/.config/xfce4 ~/.cache
+    cp -R XFCE/. ~/
+    cp XFCE/.config/user-dirs.conf ~/.config/
+    cd ..
+    rm -r DE-theming
 
-    # echo "******* All Done --- restarting Desktop Manager *******"
-    # echo "******* Please login again and enjoy EndeavourOS Theming! *******"
+echo "******* All Done --- restarting System NOW! *******"
+echo "******* Please login again and enjoy EndeavourOS Theming! *******"
 
-    yad --title="Restarting Desktop Manager" \
+    yad --title="Restarting System" \
         --text="All done --- please login again and enjoy new EndeavourOS Theming!" \
         --width=400 --height=100 \
-        --button="Restart Desktop Manager":0
+        --button="Restart System":0
 
-    sudo systemctl restart lightdm
+    sudo systemctl reboot
 
 }
 
